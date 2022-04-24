02c712114060d2f83a78e199af4ab6678251306d
Jonathon Byrdziak <jonathonbyrd@gmail.com>
2022-04-23 20:56:58 -0700

    Removed incorrect commenting

--- src/Commands/MoveFilesAutoOrganize.php
+++ src/Commands/MoveFilesAutoOrganize.php
@@ -34,17 +34,41 @@ namespace Datamelt\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+use Datamelt\Helpers\Dir;
+use Datamelt\Helpers\Shell;
 
-Class CompletionOverride extends Command {
-
-    protected static $defaultName = 'completion';
+Class MoveFilesAutoOrganize extends Command 
+{
+    protected static $defaultName = 'move:files:autoorganize';
+    protected static $defaultDescription = 'Recursively move files and automatically';
 
     protected function configure(): void
     {
         // ...
         $this
-            ->setHidden(true)
+            // the command help shown when running the command with the "--help" option
+            ->setHelp(<<<HELP
+            Recursively search through the source directory and find all files according to your glob.
+
+            Move these files to the destination directory and automatically organize them according to the chose organization method.
+            Organization Methods:
+                +--------+-------------------------------------+
+                | Option | Description                         |
+                +--------+-------------------------------------+
+                | 1      | Subfolders beginning with the first |
+                |        | letter of the filename. Filenames   |
+                |        | are created in cronological order.  |
+                +--------+-------------------------------------+
+            HELP)
+        ;
+        $this
+            // configure an argument
+            ->addArgument('source', InputArgument::REQUIRED, 'Source path')
+            ->addArgument('destination', InputArgument::REQUIRED, 'Destination path')
+            ->addArgument('organization', InputArgument::OPTIONAL, 'Organization option', '1')
+            // ...
         ;
     }
 
@@ -56,6 +80,13 @@ Class CompletionOverride extends Command {
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $source = $input->getArgument('source');
+        $dest = $input->getArgument('destination');
+        $organization = $input->getArgument('organization', 1);
+
+        $command = BIN_DIR."Move/autoorganize '$source' '$dest' $organization";
+        Shell::passthru("");
+
         return Command::SUCCESS;
     }
 
